91e2125034e4a447011dd9545df7201618196696
ivan-pl <pleskaiv@gmail.com>
2022-10-25 17:47:30 +0300

    fix: fix webpack config

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,6 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-n;
 
 module.exports = merge(common, {
   mode: "production",
