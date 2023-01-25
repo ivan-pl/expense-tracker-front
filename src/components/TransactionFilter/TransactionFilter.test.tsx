833993716061b1cff974450e891971b26367d1b9
ivan-pl <pleskaiv@gmail.com>
2023-01-25 12:33:23 +0300

    feat: setup Filter layout

--- src/components/TransactionFilter/TransactionFilter.test.tsx
+++ src/components/TransactionFilter/TransactionFilter.test.tsx
@@ -3,11 +3,17 @@ import { render, screen } from "@testing-library/react";
 
 import TransactionFilter from "./TransactionFilter";
 
+const CURRENT_DATE = "2022-11-25";
+jest.useFakeTimers().setSystemTime(new Date(CURRENT_DATE));
+
 describe("TransactionFilter", () => {
   it("renders filter", () => {
     render(<TransactionFilter />);
     expect(screen.getByPlaceholderText(/search by /i)).toBeInTheDocument();
-    expect(screen.getByText("Latest")).toBeInTheDocument();
-    expect(screen.getByText("For the period")).toBeInTheDocument();
+    expect(screen.getByPlaceholderText("Date from")).toBeInTheDocument();
+    expect(screen.getByPlaceholderText("Date to")).toBeInTheDocument();
+    expect((screen.getByTestId("filterDateTo") as HTMLInputElement).value).toBe(
+      CURRENT_DATE
+    );
   });
 });
--- src/components/TransactionFilter/TransactionFilter.tsx
+++ src/components/TransactionFilter/TransactionFilter.tsx
@@ -1,34 +1,54 @@
-import React, { FC, DetailedHTMLProps, HTMLAttributes } from "react";
+import React, { FC, DetailedHTMLProps, HTMLAttributes, useState } from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+
+import formatDate from "../../utils/formatDate";
 import "./TransactionFilter.scss";
 
 type Props = DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>;
 
 const TransactionFilter: FC<Props> = ({ className }) => {
-  return (
-    <div className={"filter " + className}>
-      <InputPattern />
-      <FilterTime />
-    </div>
-  );
-};
+  const [dateFrom, setDateFrom] = useState("");
+  const [dateTo, setDateTo] = useState(formatDate(new Date()));
+  const [pattern, setPattern] = useState("");
 
-const InputPattern: FC = () => {
   return (
-    <input
-      type="text"
-      id="filter-pattern"
-      className="filter-pattern"
-      placeholder="Search by name, amount, comment"
-    />
-  );
-};
+    <Row className={"my-3 mb-md-0 " + className}>
+      <Form.Floating as={Col} xs="12" lg="6" className="mb-3 mb-lg-0">
+        <Form.Control
+          id="filterPattern"
+          type="text"
+          placeholder="Search by Tag, amount, comment"
+          value={pattern}
+          onChange={(e) => setPattern(e.target.value)}
+        />
+        <label htmlFor="filterPattern">Search by Tag, amount, comment</label>
+      </Form.Floating>
 
-const FilterTime: FC = () => {
-  return (
-    <ul className="periods">
-      <li className="periods__item periods__item--active">Latest</li>
-      <li className="periods__item ">For the period</li>
-    </ul>
+      <Form.Floating as={Col} className="mb-3 mb-md-0" xs="6" lg="3">
+        <Form.Control
+          id="filterDateFrom"
+          type="date"
+          placeholder="Date from"
+          value={dateFrom}
+          onChange={(e) => setDateFrom(e.target.value)}
+        />
+        <label htmlFor="filterDateFrom">Date from</label>
+      </Form.Floating>
+
+      <Form.Floating as={Col} className="mb-3 mb-md-0" xs="6" lg="3">
+        <Form.Control
+          id="filterDateTo"
+          data-testid="filterDateTo"
+          type="date"
+          placeholder="Date to"
+          value={dateTo}
+          onChange={(e) => setDateTo(e.target.value)}
+        />
+        <label htmlFor="filterDateTo">Date to</label>
+      </Form.Floating>
+    </Row>
   );
 };
 
