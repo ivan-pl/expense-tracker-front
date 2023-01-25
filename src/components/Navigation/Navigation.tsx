20f123c029acf6194f109847e7281f8473ca7395
ivan-pl <pleskaiv@gmail.com>
2023-01-25 22:49:02 +0300

    feat: add active navbar

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,23 +1,39 @@
 import React, { FC } from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 import "./Navigation.scss";
 
 const Navigation: FC = () => {
+  const activeStyle = {
+    textDecoration: "underline",
+  };
+
   return (
     <nav className="nav" data-testid="nav">
-      <Link className="link link-expenses" to={"/expenses"}>
+      <NavLink
+        className="link link-expenses"
+        to={"/expenses"}
+        style={({ isActive }) => (isActive ? activeStyle : undefined)}
+      >
         <span className="link-text">Expenses</span>
-      </Link>
-      <Link className="link link-analytics" to={"/analytics"}>
+      </NavLink>
+      <NavLink
+        className="link link-analytics"
+        to={"/analytics"}
+        style={({ isActive }) => (isActive ? activeStyle : undefined)}
+      >
         <span className="link-text">Analytics</span>
-      </Link>
-      <Link className="link link-about" to={"/about"}>
+      </NavLink>
+      <NavLink
+        className="link link-about"
+        to={"/about"}
+        style={({ isActive }) => (isActive ? activeStyle : undefined)}
+      >
         <span className="link-text">About</span>
-      </Link>
-      <Link className="link link-logout" to={"/auth"}>
+      </NavLink>
+      <NavLink className="link link-logout" to={"/auth"}>
         <span className="link-text">Sign out</span>
-      </Link>
+      </NavLink>
     </nav>
   );
 };
