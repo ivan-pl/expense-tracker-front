7429f5c6012ea00bbe8fad8979a0dd3070d999f8
ivan-pl <pleskaiv@gmail.com>
2023-01-30 16:02:19 +0300

    feat: add SignUp form

--- src/components/SignUp/SignUp.tsx
+++ src/components/SignUp/SignUp.tsx
@@ -1,7 +1,106 @@
-import React, { FC } from "react";
+import React, { FC, useRef } from "react";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
 
 const SignUp: FC = () => {
-  return <div>Sign Up</div>;
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const password = useRef({});
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log("data: ", data);
+
+  return (
+    <Form onSubmit={handleSubmit(onSubmit)}>
+      <Form.Group as={Row} className="mb-3" controlId="formHorizontalEmail">
+        <Form.Label column sm={2}>
+          Email
+        </Form.Label>
+        <Col sm={10}>
+          <Form.Control
+            type="email"
+            placeholder="Email"
+            {...register("email", {
+              required: "Please enter your email",
+              pattern: {
+                value: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
+                message: "Please enter a valid email",
+              },
+            })}
+          />
+        </Col>
+        {errors.email && (
+          <Form.Text className="text-danger">{errors.email.message}</Form.Text>
+        )}
+      </Form.Group>
+
+      <Form.Group as={Row} className="mb-3" controlId="formHorizontalPassword">
+        <Form.Label column sm={2}>
+          Password
+        </Form.Label>
+        <Col sm={10}>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            {...register("password", {
+              required: "Password is required.",
+              minLength: {
+                value: 6,
+                message: "Password should be at-least 6 characters.",
+              },
+            })}
+          />
+        </Col>
+        {errors.password && (
+          <p className="text-danger">{errors.password.message}</p>
+        )}
+      </Form.Group>
+
+      <Form.Group
+        as={Row}
+        className="mb-3"
+        controlId="formHorizontalRetryPassword"
+      >
+        <Form.Label column sm={2}>
+          Confirm password
+        </Form.Label>
+        <Col sm={10}>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            {...register("confirmPassword", {
+              required: "Password is required.",
+              minLength: {
+                value: 6,
+                message: "Password should be at-least 6 characters.",
+              },
+            })}
+          />
+        </Col>
+        {errors.confirmPassword && (
+          <p className="text-danger">{errors.confirmPassword.message}</p>
+        )}
+      </Form.Group>
+
+      <Form.Group as={Row} className="mb-3">
+        <Col sm={{ span: 10, offset: 2 }}>
+          <Button type="submit">Sign Up</Button>
+        </Col>
+      </Form.Group>
+    </Form>
+  );
 };
 
 export default SignUp;
