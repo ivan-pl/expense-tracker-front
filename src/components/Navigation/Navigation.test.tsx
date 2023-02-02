09916952853db47836115d2d48f399ae041d8aae
ivan-pl <pleskaiv@gmail.com>
2023-02-02 17:35:25 +0300

    test: add Provider to Navigation

--- src/components/Navigation/Navigation.test.tsx
+++ src/components/Navigation/Navigation.test.tsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { screen, render } from "@testing-library/react";
 import { HashRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
+import store from "../../store";
 import Navigation from "./Navigation";
 
 describe("Navigation", () => {
   it("renders nav menu", async () => {
     const navBars = ["Expenses", "Analytics", "About", "Sign out"];
     render(
-      <HashRouter>
-        <Navigation />
-      </HashRouter>
+      <Provider store={store}>
+        <HashRouter>
+          <Navigation />
+        </HashRouter>
+      </Provider>
     );
 
     for (const navTitle of navBars) {
